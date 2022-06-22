6dc91b49bb8c15643a3cbc56c4408f6c0a750b9c
Niraj Uprety <nerajupreti@gmail.com>
2022-06-22 21:22:42 +0545

    inital comment

---
+++ WarehouseManagementSystem/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManagementSystem.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult login()
+        {
+            return View();
+        }
+        public ViewResult signup()
+        {
+            return View();
+        }
+        public ViewResult dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ WarehouseManagementSystem/Views/auth/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WarehouseManagementSystem.Views.auth
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
